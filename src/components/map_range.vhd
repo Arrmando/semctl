e14813117154f93f80502780d5fc3e24c779ea98
Pedro Armando <pedro.arrmando@gmail.com>
2025-11-27 23:00:14 -0300

    Adequação dos componetes car_detector e map_range

--- src/components/map_range.vhd
+++ src/components/map_range.vhd
@@ -1,10 +1,9 @@
 -- map_range
 -- Recebe uma entranda signed de N bits
 -- Transforma em uma saida signed de M bits
--- A saida se relaciona proporcionalmente com a entrada
+-- C é a diferença abs de bits entre N e M
 -- Idealmente, M tem 5 bits podendo influenciar em no máximo 16s o tempo do sinal
 -- Vamos adotar o uso de  "generic"  para poder alterar esse valor conforme necessário
-
 -- Da forma em que está escrito o código precisa de uma saida M de no mínimo 4 bits
   
 library ieee;
@@ -14,7 +13,8 @@ use ieee.numeric_std.all;
 
 entity map_range is
   generic (
-      N : integer := 10;
+  	  C : integer := 2;
+      N : integer := 7;
       M : integer := 5
     );
 
@@ -27,38 +27,15 @@ end entity;
 
 
 ARCHITECTURE Behavioral OF map_range IS
+
+signal en0: std_logic_vector(M-1 downto 0); --vetor do enable 
+signal saida: std_logic_vector(M-1 downto 0);--signal para saida
+
 BEGIN
-    PROCESS(a, e0) 
-        VARIABLE Max_SN_val : INTEGER;
-        VARIABLE Max_SM_val : INTEGER;
-        VARIABLE A_val      : INTEGER;
-        VARIABLE S_val      : INTEGER;
-    BEGIN
+    
+  en0 <= (others => e0);
+  saida <= std_logic_vector(a(N-1 downto 2));
+  s <= signed(saida and en0);
         
-        IF e0 = '1' THEN -- calcula o resultado da saida
-            
-            -- Lógica de Escalonamento 
-            A_val := TO_INTEGER(ABS(a)); 
-            Max_SN_val := 2**(N-1) - 1; 
-            Max_SM_val := 2**(M-1) - 1;
-            
-            IF Max_SN_val /= 0 THEN
-                S_val := (A_val * Max_SM_val) / Max_SN_val;
-            ELSE
-                S_val := 0; 
-            END IF;
-
-            -- Atribuição de Sinal 
-            IF a(N-1) = '1' THEN
-                S <= TO_SIGNED(-S_val, M);
-            ELSE
-                S <= TO_SIGNED(S_val, M);
-            END IF;
-
-        ELSE --zera a saida
-          S <= (OTHERS => '0'); 
-            
-        END IF;
-        
-    END PROCESS;
+    
 END ARCHITECTURE Behavioral;
